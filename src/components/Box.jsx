0aae70d1ca11332df2a9f91d06c0a37af956cd0f
leeboa2005 <leeboa2005@naver.com>
2024-05-22 20:31:45 +0900

    ex2 과제 제출

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 // TODO: props로 받은 $active 에 따라 배경색이 blue 또는 gray가 되도록 해보세요.
 const StyledBox = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color: "gray";
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  color: white;
-  font-size: 20px;
+    width: 100px;
+    height: 100px;
+    background-color: ${(props) => (props.$active ? 'blue' : 'darkgray')};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    color: white;
+    font-size: 20px;
 `;
-
+// Box 를 클릭하면 해당 Box만 파란색이 되도록 해야 합니다. components/ 안의 컴포넌트들안에서 TODO를 확인하세요.
 function Box({ active, onClick }) {
-  return <StyledBox $active={active} onClick={onClick} />;
+    return <StyledBox $active={active} onClick={onClick} />;
 }
 
 export default Box;
--- src/components/BoxContainer.jsx
+++ src/components/BoxContainer.jsx
@@ -1,27 +1,27 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import Box from "./Box";
+import { useState } from 'react';
+import styled from 'styled-components';
+import Box from './Box';
 
 const Container = styled.div`
-  display: flex;
-  gap: 10px;
+    display: flex;
+    gap: 10px;
 `;
 
 function BoxContainer() {
-  const [activeIndex, setActiveIndex] = useState(0);
+    const [activeIndex, setActiveIndex] = useState(null); // 상제 맨처음 초기화 0으로 설정할시에 맨처음 박스배경이 클릭전에 파란색으로 보인다.
 
-  const handleClick = (index) => {
-    setActiveIndex(index);
-  };
+    const handleClick = (index) => {
+        setActiveIndex(index);
+    };
 
-  return (
-    <Container>
-      {/* TODO: active prop 을 어떻게 해야 클릭한 박스임을 알수있을 지 active prop에 할당할 값을 수정해보세요. */}
-      {[0, 1, 2, 3, 4].map((index) => (
-        <Box key={index} active={false} onClick={() => handleClick(index)} />
-      ))}
-    </Container>
-  );
+    return (
+        <Container>
+            {/* TODO: active prop 을 어떻게 해야 클릭한 박스임을 알수있을 지 active prop에 할당할 값을 수정해보세요. */}
+            {[0, 1, 2, 3, 4].map((index) => (
+                <Box key={index} active={activeIndex === index} onClick={() => handleClick(index)} />
+            ))}
+        </Container>
+    );
 }
 
 export default BoxContainer;
