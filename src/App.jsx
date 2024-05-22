e1959b3a767d00da64443ac2dc79c8fedd1ecf9c
leeboa2005 <leeboa2005@naver.com>
2024-05-22 21:14:28 +0900

    ex3 과제 제출

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
-import React, { useState, useEffect } from "react";
-import TextInput from "./components/TextInput";
+import { useState, useEffect } from 'react';
+import TextInput from './components/TextInput';
 
 function App() {
-  // TODO: 로컬 스토리지에서 초기 상태로 사용할 값을 가져오세요. 새로고침 해도 기존 상태를 유지하는 것이 목적입니다.
-  // 로컬스토리지에 값이 없을 경우 빈배열[] 로 설정하세요.
-  const [texts, setTexts] = useState([]);
+    // TODO: 로컬 스토리지에서 초기 상태로 사용할 값을 가져오세요. 새로고침 해도 기존 상태를 유지하는 것이 목적입니다.
+    // 로컬스토리지에 값이 없을 경우 빈배열[] 로 설정하세요.
+    const [texts, setTexts] = useState(() => {
+        const localData = localStorage.getItem('localData');
+        return localData ? JSON.parse(localData) : [];
+    });
 
-  useEffect(() => {
-    // TODO: 상태가 변경될 때마다 로컬 스토리지에 저장. key 값은 texts 로 합시다.
-  }, [texts]);
+    useEffect(() => {
+        // TODO: 상태가 변경될 때마다 로컬 스토리지에 저장. key 값은 texts 로 합시다.
+        window.localStorage.setItem('texts', JSON.stringify(texts));
+    }, [texts]);
 
-  const onAddText = (text) => {
-    setTexts((prevTexts) => [...prevTexts, text]);
-  };
+    const onAddText = (text) => {
+        setTexts((prevTexts) => [...prevTexts, text]);
+    };
 
-  return (
-    <div>
-      <h1>Text Input and Listing</h1>
-      <TextInput onAddText={onAddText} />
-      <ul>
-        {texts.map((text, index) => (
-          <li key={index}>{text}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Text Input and Listing</h1>
+            <TextInput onAddText={onAddText} />
+            <ul>
+                {texts.map((text, index) => (
+                    <li key={index}>{text}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
